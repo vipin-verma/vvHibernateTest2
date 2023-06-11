3fb052670eeaf83f5d97dfaaf10d99883f004709
vipin-verma <vipinrnverma@gmail.com>
2023-06-11 18:24:15 +0530

    Initial commit

---
+++ src/main/java/com/example/vvHibernateTest2/VvHibernateTest2Application.java
@@ -0,0 +1,13 @@
+package com.example.vvHibernateTest2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VvHibernateTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VvHibernateTest2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vvHibernateTest2/VvHibernateTest2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vvHibernateTest2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VvHibernateTest2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
